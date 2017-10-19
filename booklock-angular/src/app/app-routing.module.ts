c5c3a5d293251dcabf6849ed596ccc6c8a7f24e5
tylertaylor <vikingtyty@gmail.com>
2017-10-19 13:38:26 -0500

    added angular route to upload component

--- booklock-angular/src/app/app-routing.module.ts
+++ booklock-angular/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { AuthGuard } from './guards/auth.guard';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
+import { UploadComponent } from './upload/upload.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -13,6 +14,11 @@ const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'upload',
+    component: UploadComponent,
     canActivate: [AuthGuard]
   }
 ];
