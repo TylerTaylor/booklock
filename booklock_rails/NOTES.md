e63dc1399904a217984a87f05dfd6d42b22d314d
tylertaylor <vikingtyty@gmail.com>
2017-10-19 12:51:54 -0500

    bookmarks controller fleshed out, routes added to routes.rb

---
+++ booklock_rails/NOTES.md
@@ -0,0 +1,13 @@
+# Models
+
+[x] - User
+[] - Bookmark
+   - name: string
+   - url: string
+   - reading_list: boolean
+   - user: references (bookmark belongs to user, user has many bookmarks)
+
+# Controllers
+
+[] - Bookmarks
+
---
+++ booklock_rails/app/controllers/bookmarks_controller.rb
@@ -0,0 +1,50 @@
+class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: [:show, :update, :destroy]
+
+  # GET /bookmarks
+  def index
+    @bookmarks = Bookmark.all
+    render json: @bookmarks
+  end
+
+  # GET /bookmarks/1
+  def show
+    render json: @bookmark
+  end
+
+  # POST /bookmarks
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+
+    if @bookmark.save
+      render json: @bookmark, status: :created, location: @bookmark
+    else
+      render json: @bookmark.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /bookmarks/1
+  def update
+    if @bookmark.update(bookmark_params)
+      render json: @bookmark
+    else
+      render json: @bookmark.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /bookmarks/1
+  def destroy
+    @bookmark.destroy
+  end
+
+  private
+
+    def set_bookmark
+      @bookmark = Bookmark.find(params[:id])
+    end
+
+    def bookmark_params
+      params.require(:bookmark).permit(:name, :url, :reading_list)
+    end
+
+end
