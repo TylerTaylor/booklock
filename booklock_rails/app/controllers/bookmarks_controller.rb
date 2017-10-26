0eacd9ab5af2b65c0545da913b6377e3ffb007f0
tylertaylor <vikingtyty@gmail.com>
2017-10-26 09:59:06 -0500

    added plist gem to handle the plist file, file upload is currently WORKING! angular isn't grabbing the newly created links yet but rails is successfully adding items to the DB

--- booklock_rails/app/controllers/bookmarks_controller.rb
+++ booklock_rails/app/controllers/bookmarks_controller.rb
@@ -42,7 +42,52 @@ class BookmarksController < ApplicationController
 
     bookmarks_xml = `plutil -convert xml1 -o - #{path}`
 
-    binding.pry
+    # We also need to do this for actual bookmarks, not just reading list
+    build_reading_list(bookmarks_xml)
+  end
+
+  def build_reading_list(plist_file)
+    begin
+      # first we parse the xml
+      # grab the "Children"
+      # select the ones with the title "ReadingList"
+      # get THOSE child elements
+      # collect all the URLStrings
+      reading_list_objects = Plist.parse_xml(plist_file)["Children"].select{|e|e["Title"]=="com.apple.ReadingList"}[0]["Children"]
+
+      new_reading_list = []
+
+      reading_list_objects.each do |obj|
+        new_bookmark = {}
+
+        # For each link object we have
+        obj.each do |key, val|
+          
+          # Two separate if statements because we want both.. is this the best way to go about it?
+          if key == "URIDictionary"
+            new_bookmark["title"] = val["title"]
+          end
+
+          if key == "URLString"
+            new_bookmark["url"] = val
+          end
+
+        end # end obj.each
+
+        new_reading_list << new_bookmark
+      end
+
+    rescue NoMethodError => e
+      puts "Your reading list is empty."
+    end # /begin
+
+    # Here is where we actually create our bookmark objects
+    # TODO: This needs to be sorted out, I think it should differentiate reading list from bookmarks.
+    new_reading_list.each do |bookmark|
+      binding.pry
+      Bookmark.create(name: bookmark["title"], url: bookmark["url"], reading_list: true)
+    end
+
   end
 
   private
