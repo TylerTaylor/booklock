3a71241e34a32c4d404923602bf1b4e3d91ca2a4
tylertaylor <vikingtyty@gmail.com>
2017-11-01 10:30:09 -0500

    set bookmarks component to pass our auth headers to rails to get our user's bookmarks

--- booklock-angular/src/app/bookmarks/bookmarks.component.ts
+++ booklock-angular/src/app/bookmarks/bookmarks.component.ts
@@ -1,3 +1,4 @@
+import { Angular2TokenService } from 'angular2-token';
 import { Component, OnInit } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 
@@ -13,13 +14,21 @@ export class BookmarksComponent implements OnInit {
 
   loading: boolean = false;
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, private authService: Angular2TokenService) { }
 
   ngOnInit() {
     // Starts our loading spinner, which disappears when our data loads 
     this.loading = true;
 
-    this.http.get('http://localhost:3000/bookmarks.json')
+    let headers = new Headers();
+    headers.append("access-token", this.authService.currentAuthData["accessToken"])
+    headers.append("expiry", this.authService.currentAuthData["expiry"])
+    headers.append("token-type", this.authService.currentAuthData["tokenType"])
+    headers.append("uid", this.authService.currentAuthData["uid"])
+    headers.append("client", this.authService.currentAuthData["client"])
+    let options = new RequestOptions({ headers: headers })
+
+    this.http.get('http://localhost:3000/bookmarks.json', options)
       .subscribe(res => {
         this.loading = false;
         this.bookmarks = res.json();
