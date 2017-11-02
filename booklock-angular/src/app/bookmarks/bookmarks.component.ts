6aad6ad14ae23c167a9147bce785585913969783
tylertaylor <vikingtyty@gmail.com>
2017-11-02 13:35:08 -0500

    delete now working! changed the card layout a little bit, from anchor tags to buttons since they don't actually navigate.

--- booklock-angular/src/app/bookmarks/bookmarks.component.ts
+++ booklock-angular/src/app/bookmarks/bookmarks.component.ts
@@ -35,4 +35,25 @@ export class BookmarksComponent implements OnInit {
       })
   }
 
+  deleteBookmark(id) {
+    if(confirm("Are you sure you want to delete this bookmark?")){
+      console.log("We can send a delete request to rails here. Remember to pass in the auth headers. Probably should refactor this component now.")
+
+      // TODO: Should probably clean up this repetitive code
+      let headers = new Headers();
+      headers.append("access-token", this.authService.currentAuthData["accessToken"])
+      headers.append("expiry", this.authService.currentAuthData["expiry"])
+      headers.append("token-type", this.authService.currentAuthData["tokenType"])
+      headers.append("uid", this.authService.currentAuthData["uid"])
+      headers.append("client", this.authService.currentAuthData["client"])
+      let options = new RequestOptions({ headers: headers })
+
+      // First delete from rails backend, then delete from our JS bookmarks collection
+      this.http.delete('http://localhost:3000/bookmarks/' + id, options)
+        .subscribe(res => {
+          this.bookmarks = this.bookmarks.filter(bookmark => bookmark.id !== id)
+        })
+    }
+  }
+
 }
